a6368b5f2dfdc8ddc8650731c3b9c407d005e7d0
radioactive17 <jigsshah.97@gmail.com>
2023-06-13 22:31:37 -0400

    Day 1

---
+++ index.js
@@ -0,0 +1,33 @@
+console.log('Hello World');
+
+// Section 1: Variables (Use camel casing)
+// First character must be letter or _ or $
+// Variable names are case sensitive
+
+var myName = 'Jignesh';
+var myAge = 18;
+var myFlag = true;
+
+// Section 2: DataTypes
+// primitives: undefined, boolean, number, string, bigint, symbol
+console.log(typeof(unassignedVar)); 
+
+console.log(13 + '37');
+console.log(13 - '5');
+console.log('Java' + ' Script');
+console.log(' ' + 0);
+console.log('vinod' - 'thapa');
+console.log(true + true);
+console.log(true + false);
+console.log(false - true);
+
+// null vs undefined
+var nullVar = null;
+var someVar;
+console.log(typeof(someVar));
+console.log(typeof(nullVar));
+console.log(someVar);
+
+
+console.log(isNaN(nullVar));
+console.log(isNaN(someVar));
