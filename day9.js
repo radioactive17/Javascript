4baf5f1ccabeb4a72c849202837f23a5c7780faa
radioactive17 <jigsshah.97@gmail.com>
2023-10-17 12:11:56 -0400

    Events in Js

---
+++ day9.js
@@ -0,0 +1,17 @@
+// Section 12: Events
+// events happens to HTML elements
+// an html event is something the browser or user does
+
+// 4 ways of writing events in JS
+// 1. using inline events alert();
+// 2. By Calling a function 
+// 3. using Inline events (HTML onclick = "" property and element.onclick) cannot be called multiple times (console.log & alert )
+// 4. using Event Listners (addEventListner and IE's attachEvent) can be called multiple times (console.log & aler)
+// See EventTypes.html for reference
+
+// Event object is the parent of event object (E vs e)
+// See EventListner.html
+
+
+// addEventListner does not override existing event handler whereas onclick overrides exisiting event handler
+// onlcick works everywhere whereas addEventListner doesn't work in Internet Explorer before version 9.
